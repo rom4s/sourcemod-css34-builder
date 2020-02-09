a277c42286cc0d01b02de3e073c8330d9af193ce
Roman <roman@x-eu.com>
2020-02-09 17:38:37 +0500

    Oops... fix build
    cd  sourcemod before run build

--- run/linux.sh
+++ run/linux.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #
 SELF_PATH=$(realpath ${0%/*})
-python $SELF_PATH/../build.py
+echo BUILDER_PATH = ${SELF_PATH}
+cd sourcemod && python $SELF_PATH/../build.py
--- run/windows.bat
+++ run/windows.bat
@@ -1,4 +1,5 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86
 SET SELF_PATH=%~dp0%
-python %SELF_PATH:~0,-1%\..\build.py
+echo BUILDER_PATH = %SELF_PATH%
+cd sourcemod && python %SELF_PATH:~0,-1%\..\build.py
