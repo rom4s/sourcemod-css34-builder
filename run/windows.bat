1f57771796f8ae1be55b6831d0fd80e1ce4c68a1
Roman <roman@x-eu.com>
2020-02-09 18:26:30 +0500

    Update windows.bat
    Travis set env CXX=g++, remote it.

--- run/windows.bat
+++ run/windows.bat
@@ -1,5 +1,7 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86
 SET SELF_PATH=%~dp0%
+SET CXX=
+SET CC=
 echo BUILDER_PATH = %SELF_PATH%
 cd sourcemod && python %SELF_PATH:~0,-1%\..\build.py
