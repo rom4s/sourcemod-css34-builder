ad9078b1aaa05cf739ee128fb67364f34c33f088
Roman <roman@x-eu.com>
2020-02-09 18:14:08 +0500

    Update linux.sh
    2nd try

--- run/linux.sh
+++ run/linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
-SELF_PATH=${0%/*}
-echo BUILDER_PATH = $(pwd)/${SELF_PATH}
-cd sourcemod && python $SELF_PATH/../build.py
+SELF_PATH=$(python -c "import os; print(os.path.realpath('${0%/*}'))")
+echo BUILDER_PATH = ${SELF_PATH}
+cd sourcemod && python ${SELF_PATH}/../build.py
